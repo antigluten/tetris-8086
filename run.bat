1d862d2f8fbe4bbc220e718ec0022d55796354ec
Vladimir Gusev <antigluten@icloud.com>
2024-01-05 02:17:31 +0300

    Add collision and redesign

--- run.bat
+++ run.bat
@@ -1,15 +1,26 @@
 @echo off 
 
+set name=program
+
 cls
 
-ml /Zi /Fl /c program.asm
+ml /Zi /Fl /c %name%.asm
 if errorlevel 1 goto error
 
-link /codeview program.obj;
+link /codeview %name%.obj;
 
 if errorlevel 1 goto error
 
-program
+IF "%1%"=="1" goto debug
+
+goto run
+
+:debug
+cv %name%
+goto end
+
+:run
+%name%
 
 :error
-
+:end
