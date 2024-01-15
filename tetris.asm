5ec759682f56eb572eb4bb77077efc60e2246fed
Vladimir Guser <antigluten@icloud.com>
2024-01-15 23:46:46 +0300

    add drawing of a figure

--- tetris.asm
+++ tetris.asm
@@ -4,6 +4,9 @@
 
 .data
 
+W_BYTE equ 1
+W_WORD equ 2
+
 VRAM               equ        0b800h
 PLAYFIELD_WIDTH    equ        10
 PLAYFIELD_HEIGHT   equ        20
@@ -18,92 +21,96 @@ RAM                byte       (FIELD_HEIGHT * FIELD_WIDTH) * 2 DUP(0)
 CHAR_DOT           equ         02eh 
 BACKGROUND         equ         00fh
 
+
+square             db         4,4,0,0
+                   db         4,4,0,0
+                   db         0,0,0,0
+                   db         0,0,0,0
+
 ; LOGIC              byte       PLAYFIELD_HEIGHT * PLAYFIELD_WIDTH DUP(0)
 
 .code
 
-@border: 
-    mov bx, 0
+@draw_figure:
+    lea di, RAM
+    mov ax, @data
+    mov es, ax
 
-    mov cx, 0
+    ;mov bx, 0
+    ;mov al, bh
+    ;mov ah, 160
+    ;mul ah
+
+    ;mov bh, 0
+    ;shl bx, 1
+
+    ;add bx, 2
+
+    lea si, square
+
+    mov bx, 4
+    @row:
+        mov cx, 4
+        @col:
+            lodsb
+            ;mov al, byte ptr ds:[si]
+            ;inc si
+            or al, al
+            jz @skip
+
+            mov ah, 0fh
+            mov al, 5bh
+            stosw
+
+            mov al, 5dh
+            stosw
+
+            loop @col
+            jmp @end
+    @skip:
+        add di, 4
+        loop @col
+    @end:
+        ;add di, 160 - 2 * 4
+        dec bx
+        jnz @row
+
+
+
+ret
+        
+@border:
+    lea di, RAM
+    mov ax, @data
+    mov es, ax
+
+    mov bx, 0
 
     mov dx, FIELD_HEIGHT - 1
-    @border_loop:
-        mov ah, bl
-        mov al, FIELD_WIDTH
-        mul ah
-        
-        mov byte ptr [RAM + ax], CHAR_DOT
-        mov byte ptr [RAM + ax + 1], BACKGROUND
-
-        add cx, 12
-
-        inc bl
-        dec dx 
-    loopnz @border_loop
-
-    ret
-
-    mov ax, @data
-    mov es, ax
-
-    lea di, RAM
-
-    mov ah, bl
-    mov al, FIELD_WIDTH
-    mul ah
-
-    shl ax, 1
-
-    add di, ax
-
-    ;mov al, CHAR_DOT
-    mov al, 03dh
-    mov ah, BACKGROUND
-    mov cx, FIELD_WIDTH
-    rep stosw
-
-    ret
-
-@mock:
-    lea di, RAM
-    mov ax, @data
-    mov es, ax
-
-    mov bx, 0
-
-    mov dx, FIELD_HEIGHT
     @@border_loop:
-
-        mov ah, bl
-        mov al, 10
-        mul ah
-
-        inc bl
-        
-        ;mov al, 0
-        add al, CHAR_DOT 
+        mov al, CHAR_DOT
         mov ah, BACKGROUND
         stosw
 
-        add di, FIELD_WIDTH * 2 - 1 * 2
+        add di, FIELD_WIDTH * 2 - 2 * W_WORD
+
+        mov al, CHAR_DOT
+        mov ah, BACKGROUND
+        stosw
 
         dec dx 
     loopnz @@border_loop
 
-    ret
-
-    mov di, 0
-    add di, FIELD_WIDTH * (FIELD_HEIGHT) * 2
-
-    mov al, 04fh
+    mov cx, FIELD_WIDTH
+    mov al, CHAR_DOT
     mov ah, BACKGROUND
-    stosw
+    rep stosw
 
     ret
 
 @copyToVRAM:
-    call @mock
+    call @border
+    call @draw_figure
 
     mov ax, VRAM
     mov es, ax
